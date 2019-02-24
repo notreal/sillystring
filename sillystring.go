e0e2c2431741ecc51e2f74f2ffa87a72265544be
Nick <nnotreall@gmail.com>
2019-02-23 19:42:04 -0800

    add flag to run webserver

--- sillystring.go
+++ sillystring.go
@@ -3,22 +3,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"strings"
 	"unicode/utf8"
 )
 
+var serve = flag.Bool("s", false, "run webserver")
+
 func main() {
 	// TODO make this a webserver
+	flag.Parse()
+	if *serve {
+		http.HandleFunc("/", handler)
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	}
 	input := strings.Join(os.Args[1:], " ")
 	fmt.Println(Acute(input))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var path = strings.Split(r.URL.Path, "/")[1:] //remove leading /
+	for _, word := range path {
+		fmt.Fprintf(w, "%s\n", Acute(word))
+	}
 }
 
 // Acute returns string with available characters replaced with acute version
 func Acute(in string) string {
 	matches := "ACEGILNORSUWYZacegilnorsuwyz"
-	accents := RuneSlicer("ÁĆÉǴÍĹŃÓŔŚÚẂÝŹáćéǵíĺńóŕśúẃýź")
+	accents := runeSlicer("ÁĆÉǴÍĹŃÓŔŚÚẂÝŹáćéǵíĺńóŕśúẃýź")
 
 	var out strings.Builder
 	for _, r := range in {
@@ -32,8 +49,8 @@ func Acute(in string) string {
 	return out.String()
 }
 
-// RuneSlicer returns rune slice given a string
-func RuneSlicer(in string) []rune {
+// runeSlicer returns rune slice given a string
+func runeSlicer(in string) []rune {
 	var out []rune
 	for len(in) > 0 {
 		r, size := utf8.DecodeRuneInString(in)
