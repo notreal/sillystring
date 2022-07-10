b927d215b48fbe38c925bd4934d07b84bd85903b
nnotreall <nnotreall@gmail.com>
2022-07-10 16:09:19 -0700

    issue 7: more translations

--- sillystring.go
+++ sillystring.go
@@ -9,53 +9,95 @@ import (
 
 // Translation maps input & output characters
 type Translation struct {
-	source string
-	dest   []rune
+	source  string
+	dest    []rune
+	reverse bool
 }
 
 func GetAllTranslations() map[string]Translation {
 	// global maps are tricky so get translations here
+	alphabet := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	return map[string]Translation{
 		// keep alphabetized
-		"acute": Translation{
+		"acute": {
 			"ACEGILNORSUWYZacegilnorsuwyz",
 			[]rune("ÁĆÉǴÍĹŃÓŔŚÚẂÝŹáćéǵíĺńóŕśúẃýź"),
+			false,
 		},
-		"caron": Translation{
+		"caron": {
 			"ACDEGHIKNORSTUZaceghijknorsuz",
 			[]rune("ǍČĎĚǦȞǏǨŇǑŘŠŤǓŽǎčěǧȟǐǰǩňǒřšǔž"),
+			false,
 		},
-		"circumflex": Translation{
+		"circle-black": {
+			alphabet,
+			[]rune("🅐🅑🅒🅓🅔🅕🅖🅗🅘🅙🅚🅛🅜🅝🅞🅟🅠🅡🅢🅣🅤🅥🅦🅧🅨🅩🅐🅑🅒🅓🅔🅕🅖🅗🅘🅙🅚🅛🅜🅝🅞🅟🅠🅡🅢🅣🅤🅥🅦🅧🅨🅩"),
+			false,
+		},
+		"circle-white": {
+			alphabet,
+			[]rune("ⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ"),
+			false,
+		},
+		"circumflex": {
 			"ACEGHIJOSUWYZaceghijosuwyz",
 			[]rune("ÂĈÊĜĤÎĴÔŜÛŴŶẐâĉêĝĥîĵôŝûŵŷẑ"),
+			false,
 		},
-		"diaeresis": Translation{
-			"AEHIOUWXYaehiotwxy",
-			[]rune("ӒËḦÏÖÜẄẌŸӓëḧïöẗẅẍÿ"),
-		},
-		"dot_above": Translation{
+		"dot_above": {
 			"ABCDEFGHIMNOPRSTWXYZabcdefghmnoprstwxyz",
 			[]rune("ȦḂĊḊĖḞĠḢİṀṄȮṖṘṠṪẆẊẎŻȧḃċḋėḟġḣṁṅȯṗṙṡṫẇẋẏż"),
+			false,
 		},
-		"dot_below": Translation{
+		"dot_below": {
 			"ABDEHIKLMNORSTUVWYZabdehiklmnorstuvwyz",
 			[]rune("ẠḄḌẸḤỊḲḶṂṆỌṚṢṬỤṾẈỴẒạḅḍẹḥịḳḷṃṇọṛṣṭụṿẉỵẓ"),
+			false,
 		},
-		"fraktur": Translation{
-			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+		"double_struck": {
+			alphabet,
+			[]rune("𝔸𝔹ℂ𝔻𝔼𝔽𝔾ℍ𝕀𝕁𝕂𝕃𝕄ℕ𝕆ℙℚℝ𝕊𝕋𝕌𝕍𝕎𝕏𝕐ℤ𝕒𝕓𝕔𝕕𝕖𝕗𝕘𝕙𝕚𝕛𝕜𝕝𝕞𝕟𝕠𝕡𝕢𝕣𝕤𝕥𝕦𝕧𝕨𝕩𝕪𝕫"),
+			false,
+		},
+		"fraktur": {
+			alphabet,
 			[]rune("𝕬𝕭𝕮𝕯𝕰𝕱𝕲𝕳𝕴𝕵𝕶𝕷𝕸𝕹𝕺𝕻𝕼𝕽𝕾𝕿𝖀𝖁𝖂𝖃𝖄𝖅𝖆𝖇𝖈𝖉𝖊𝖋𝖌𝖍𝖎𝖏𝖐𝖑𝖒𝖓𝖔𝖕𝖖𝖗𝖘𝖙𝖚𝖛𝖜𝖝𝖞𝖟"),
+			false,
 		},
-		"grave": Translation{
-			"AEINOUWYaeinouwy",
-			[]rune("ÀÈÌǸÒÙẀỲàèìǹòùẁỳ"),
+		"hook": { //tried AOXaj
+			"ABCDFGHKMNOPTVWXYZabcdfghjklmnopqrstvwxyz",
+			[]rune("ƛƁƇƊƑƓꞪƘⱮƝƠƤƬƲⱲӼƳȤąɓƈɗƒɠɧʝƙɭɱɳơƥɋɽʂƭʋⱳӽƴȥ"),
+			false,
 		},
-		"hook": Translation{
-			"BCDFGHKMNPTVWYZbcdfghklmnpstvwyz",
-			[]rune("ƁƇƊƑƓꞪƘⱮƝƤƬƲⱲƳȤɓƈɗƒɠɦƙɭɱɲƥʂƭʋⱳƴȥ"),
+		"mathematical": {
+			alphabet,
+			[]rune("𝓐𝓑𝓒𝓓𝓔𝓕𝓖𝓗𝓘𝓙𝓚𝓛𝓜𝓝𝓞𝓟𝓠𝓡𝓢𝓣𝓤𝓥𝓦𝓧𝓨𝓩𝓪𝓫𝓬𝓭𝓮𝓯𝓰𝓱𝓲𝓳𝓴𝓵𝓶𝓷𝓸𝓹𝓺𝓻𝓼𝓽𝓾𝓿𝔀𝔁𝔂𝔃"),
+			false,
 		},
-		"tilde": Translation{
-			"AEINOUVYaeinouvy",
-			[]rune("ÃẼĨÑÕŨṼỸãẽĩñõũṽỹ"),
+		"square_white": {
+			alphabet,
+			[]rune("🄰🄱🄲🄳🄴🄵🄶🄷🄸🄹🄺🄻🄼🄽🄾🄿🅀🅁🅂🅃🅄🅅🅆🅇🅈🅉🄰🄱🄲🄳🄴🄵🄶🄷🄸🄹🄺🄻🄼🄽🄾🄿🅀🅁🅂🅃🅄🅅🅆🅇🅈🅉"),
+			false,
+		},
+		"square_black": {
+			alphabet,
+			[]rune("🅰🅱🅲🅳🅴🅵🅶🅷🅸🅹🅺🅻🅼🅽🅾🅿🆀🆁🆂🆃🆄🆅🆆🆇🆈🆉🅰🅱🅲🅳🅴🅵🅶🅷🅸🅹🅺🅻🅼🅽🅾🅿🆀🆁🆂🆃🆄🆅🆆🆇🆈🆉"),
+			false,
+		},
+		"upside_down": {
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz?!&'.",
+			[]rune("ⱯꓭꓛꓷƎꓞꓨHIſꓘꓶWNOꓒῸꓤSꓕꓵꓥMX⅄Zɐqɔpǝɟƃɥᴉɾʞʃɯuodbɹsʇnʌʍxʎz¿¡⅋,˙"),
+			true,
+		},
+		"yi": {
+			alphabet,
+			[]rune("ꁲꋰꀯꂠꈼꄞꁅꍩꂑ꒻ꀗ꒒ꂵꋊꂦꉣꁷꌅꌚꋖꐇꀰꅏꇒꐞꁴꁲꋰꀯꂠꈼꄞꁅꍩꂑ꒻ꀗ꒒ꂵꋊꂦꉣꁷꌅꌚꋖꐇꀰꅏꇒꐞꁴ"),
+			false,
+		},
+		"zany": {
+			alphabet,
+			[]rune("ค๒ς๔єԲﻮђเ꒻қՆ๓ภ๏קợՐรtย౮ฬאץzค๒ς๔єԲﻮђเ꒻қՆ๓ภ๏קợՐรtย౮ฬאץz"),
+			false,
 		},
 	}
 }
@@ -86,6 +128,13 @@ func Translate(s string, t Translation) string {
 		} else {
 			out.WriteRune(r)
 		}
+	}
+	if t.reverse {
+		var result string
+		for _, r := range out.String() {
+			result = string(r) + result
+		}
+		return result
 	}
 	return out.String()
 }
